444d618d37b60d814398293a56c2837f530dc6b1
Sam Moldenhauer <sam@Sams-MacBook-Pro-2.local>
2026-02-26 16:14:14 -0500

    Ignored ds store

--- README.md
+++
@@ -1,5 +0,0 @@
-# Neuropixel Probe Documentation
-
-This is a gui meant to serve as a Neuropixel probe documentation for safe automatic storage and easy access.
-
-
--- main.py
+++
@@ -1,8 +0,0 @@
-from .utils.safety import resource_check
-from .gui.main_gui import MainGui
-
-if __name__ == '__main__':
-    resource_check()
-    root = MainGui()
-    root.mainloop()
-
